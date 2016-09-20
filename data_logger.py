5c7f5bfa8d3216b67883e89ece668eb107c3ce8f
Karl Johansson <johanssk@uci.edu>
2016-09-20 05:58:18 -0700

    Rewrite code as class

--- data_logger.py
+++ data_logger.py
@@ -20,216 +20,203 @@ logging.basicConfig(
 # logging.disable(logging.DEBUG)
 
 
-def parse_config():
-    config = ConfigObj('config.ini', configspec='configspec.ini')
+class Test(object):
+    def __init__(self):
+        config = ConfigObj('config.ini', configspec='configspec.ini')
+        validator = Validator()
+        if config.validate(validator) is True:
+            # Save options
+            self.save_path = config["Save"]["OUTPUT_SAVE_PATH"]
+            self.save_name = config["Save"]["OUTPUT_SAVE_NAME"]
+            self.save_ext = config["Save"]["OUTPUT_SAVE_EXTENTION"]
 
-    validator = Validator()
-    result = config.validate(validator)
+            # System Commands
+            self.setup_command = config["Commands"]["SETUP_COMMAND"]
+            self.send_cmd = config["Commands"]["SEND_CMD"]
 
-    if result is True:
-        # Save Options
-        save_data = {}
-        save_data["path"] = config["Save"]["OUTPUT_SAVE_PATH"]
-        save_data["name"] = config["Save"]["OUTPUT_SAVE_NAME"]
-        save_data["ext"] = config["Save"]["OUTPUT_SAVE_EXTENTION"]
+            # Sample and run time
+            self.sample_time = config["Times"]["SAMPLE_TIME"]
+            self.runtime = config["Times"]["TOTAL_RUNTIME"]
+        else:
+            print "Could not validate config file"
+            logging.critical("Could not validate config file")
+            sys.exit()
 
-        # System Commands
-        commands = {}
-        commands["setup"] = config["Commands"]["SETUP_CMD"]
-        commands["send"] = config["Commands"]["SEND_CMD"]
+    def determine_loop_count(self):
+        """
+        Used to determine number of loops the program should run
+        by dividing total_runtime and sample_time
 
-        # Sample and run time
-        times = {}
-        times["sample_time"] = config["Times"]["SAMPLE_TIME"]
-        times["runtime"] = config["Times"]["TOTAL_RUNTIME"]
+        INPUT:
+            total_runtime: float
+            sample_time: float
 
-        return save_data, commands, times
-    else:
-        print "Could not validate config file"
-        logging.critical("Could not validate config file")
-        sys.exit()
+        OUTPUT:
+            returns -1 if total_runtime is -1, num_loops otherwise
+        """
 
+        num_loops = round(self.runtime / self.sample_time) if (
+            self.runtime > 0) else -1
+        return num_loops
 
-def determine_loop_count(total_runtime, sample_time):
-    """
-    Used to determine number of loops the program should run
-    by dividing total_runtime and sample_time
+    @retry(stop_max_attempt_number=7, wait_fixed=2000)
+    def auto_connect_device(self):
+        """
+        Finds ports that are currently availiable and attempts to connect
 
-    INPUT:
-        total_runtime: float
-        sample_time: float
+        INPUT
+        None
 
-    OUTPUT:
-        returns -1 if total_runtime is -1, num_loops otherwise
-    """
+        OUTPUT
+        connect_ser if connected and device responding
+        Raise exception if no connection
+        """
+        logging.info("Connecting to device")
+        ports = list(lports.comports(
+        ))  # Change to .grep once determine what port returns
+        logging.debug(ports)
+        for com_port in ports:
+            connect_ser = serial.Serial(com_port.device, 9600, timeout=0.5)
 
-    num_loops = round(total_runtime / sample_time) if (
-        total_runtime > 0) else -1
-    return num_loops
-
-
-def collect_data(my_ser, send, sample, run_loops):
-    """
-    Sets up device and sends commands, then reads response
-
-    INPUT
-    my_ser = serial connection
-
-    OUTPUT
-    out: List of tuples of format (time_of_reading, reading)
-    """
-    # Assigns variables from configuration file to local variables
-    # Used to speed up while loop
-
-    logging.debug("Send command: %s", send)
-    logging.debug("Sample time: %s", sample)
-
-    # Initialize list to contain readings
-    out = []
-
-    # Assign function lookups to variables
-    # Used to speed up while loop
-    current_time = time.time
-    write = my_ser.write
-    read = my_ser.read
-    rstrip = str.rstrip
-    sleep = time.sleep
-    append = out.append
-
-    logging.info("Beginning data logging")
-
-    try:
-        relative_time_offset = current_time()
-        for run_count in itertools.count():
-            if run_count == run_loops:
-                break
-            logging.debug("Run count: %s", run_count)
-            start_time = current_time()
-
-            logging.info("Sending command: %s", send)
-            write("%s\n" % send)
-
-            return_string = rstrip(str(read(256)))
-            logging.info("Return string: %s", return_string)
-
+            # Send command to ensure device is responding
+            # and connected to correct port
+            logging.info("Inputting device settings to: %s", com_port.device)
+            logging.info("Setup settings: %s", self.setup_command)
+            connect_ser.write("%s\n" % self.setup_command)
+            connect_ser.write("%s\n" % self.send_cmd)
+            return_string = str(connect_ser.read(256)).rstrip()
+            logging.debug(return_string)
             if len(return_string) > 0:
-                print return_string
-                append((current_time() - relative_time_offset, return_string))
-            else:
-                logging.critical("No response from system")
-                raise error_codes.ReturnError
+                return connect_ser
+        logging.warning("No connection to device")
+        raise error_codes.ConnectError
 
-            offset = current_time() - start_time
-            logging.debug("Offset: %f", offset)
-            if sample - offset > 0:
-                sleep(sample - offset)
-            print current_time() - start_time
-    except KeyboardInterrupt:
-        pass
-    logging.info("Done collecting data")
-    return out
+    def collect_data(self, my_ser, run_loops):
+        """
+        Sets up device and sends commands, then reads response
 
+        INPUT
+        my_ser = serial connection
 
-def write_file(out, save_data):
-    """
-    Writes data to specified file
+        OUTPUT
+        out: List of tuples of format (time_of_reading, reading)
+        """
+        # Assigns variables from configuration file to local variables
+        # Used to speed up while loop
 
-    INPUT
-    out: List of tuples of format (time_of_reading, reading)
+        logging.debug("Send command: %s", self.send_cmd)
+        logging.debug("Sample time: %s", self.sample_time)
 
-    OUTPUT
-    File at location LOG_SAVE_PATH, with filename of format
-    "LOG_SAVE_NAME %Y-%m-%d %H_%M_%S" and extension LOG_SAVE_EXTENTION
-    """
-    logging.info("Saving values to file")
-    now = datetime.datetime.now()
-    file_time = now.strftime("%Y-%m-%d %H_%M_%S")
+        # Initialize list to contain readings
+        out = []
 
-    filename = "%s %s%s" % (save_data["name"], file_time, save_data["ext"])
-    logging.debug(filename)
+        # Assign function lookups to variables
+        # Used to speed up while loop
+        current_time = time.time
+        write = my_ser.write
+        read = my_ser.read
+        rstrip = str.rstrip
+        sleep = time.sleep
+        append = out.append
 
-    if os.environ.has_key("USERPROFILE"):
-        user_path = os.environ["USERPROFILE"]
-    else:
-        user_path = os.path.expanduser("~")
-    save_path = os.path.join(user_path, save_data["path"])
-    full_filename = os.path.join(save_path, filename)
-    logging.info("Saving as: %s", full_filename)
+        logging.info("Beginning data logging")
 
-    with open(full_filename, 'a+') as data:
-        for pair in tqdm.tqdm(out):
-            write_line = "%s,%s\n" % (str(pair[0]), str(pair[1]))
-            data.write(write_line)
-    return full_filename
+        try:
+            relative_time_offset = current_time()
+            for run_count in itertools.count():
+                if run_count == run_loops:
+                    break
+                logging.debug("Run count: %s", run_count)
+                start_time = current_time()
 
+                logging.info("Sending command: %s", self.send_cmd)
+                write("%s\n" % self.send_cmd)
 
-@retry(stop_max_attempt_number=7, wait_fixed=2000)
-def auto_connect_device(setup, send):
-    """
-    Finds ports that are currently availiable and attempts to connect
+                return_string = rstrip(str(read(256)))
+                logging.info("Return string: %s", return_string)
 
-    INPUT
-    None
+                if len(return_string) > 0:
+                    print return_string
+                    append(
+                        (current_time() - relative_time_offset, return_string))
+                else:
+                    logging.critical("No response from system")
+                    raise error_codes.ReturnError
 
-    OUTPUT
-    connect_ser if connected and device responding
-    Raise exception if no connection
-    """
-    logging.info("Connecting to device")
-    ports = list(
-        lports.comports())  # Change to .grep once determine what port returns
-    logging.debug(ports)
-    for com_port in ports:
-        connect_ser = serial.Serial(com_port.device, 9600, timeout=0.5)
+                offset = current_time() - start_time
+                logging.debug("Offset: %f", offset)
+                if self.sample_time - offset > 0:
+                    sleep(self.sample_time - offset)
+                print current_time() - start_time
+        except KeyboardInterrupt:
+            pass
+        logging.info("Done collecting data")
+        return out
 
-        # Send command to ensure device is responding
-        # and connected to correct port
-        logging.info("Inputting device settings to: %s", com_port.device)
-        logging.info("Setup settings: %s", setup)
-        connect_ser.write("%s\n" % setup)
-        connect_ser.write("%s\n" % send)
-        return_string = connect_ser.read(256)
-        return_string = str(return_string).rstrip()
-        logging.debug(return_string)
-        if len(return_string) > 0:
-            return connect_ser
-    logging.warning("No connection to device")
-    raise error_codes.ConnectError
+    def write_file(self, out):
+        """
+        Writes data to specified file
+
+        INPUT
+        out: List of tuples of format (time_of_reading, reading)
+
+        OUTPUT
+        File at location LOG_SAVE_PATH, with filename of format
+        "LOG_SAVE_NAME %Y-%m-%d %H_%M_%S" and extension LOG_SAVE_EXTENTION
+        """
+        logging.info("Saving values to file")
+        now = datetime.datetime.now()
+        file_time = now.strftime("%Y-%m-%d %H_%M_%S")
+
+        filename = "%s %s%s" % (self.save_name, file_time, self.save_ext)
+        logging.debug(filename)
+
+        if os.environ.has_key("USERPROFILE"):
+            user_path = os.environ["USERPROFILE"]
+        else:
+            user_path = os.path.expanduser("~")
+        save_path = os.path.join(user_path, self.save_path)
+        full_filename = os.path.join(save_path, filename)
+        logging.info("Saving as: %s", full_filename)
+
+        with open(full_filename, 'a+') as data:
+            for pair in tqdm.tqdm(out):
+                write_line = "%s,%s\n" % (str(pair[0]), str(pair[1]))
+                data.write(write_line)
+        return full_filename
+
+    def run_full(self):
+        try:
+            try:
+                START_TOTAL_TIME = time.time()
+                ser = self.auto_connect_device()
+                READ_TIME_START = time.time()
+                output = self.collect_data(ser, self.determine_loop_count())
+                READ_TIME_TOTAL = time.time() - READ_TIME_START
+                print "Total time sampled: %s" % str(READ_TIME_TOTAL)
+                self.write_file(output)
+                ser.close()
+            except error_codes.ConnectError:
+                logging.critical("Unable to connect to device")
+                sys.exit()
+            except error_codes.ReturnError:
+                logging.critical("Exiting program")
+                sys.exit()
+            print "Total Time: %s" % (time.time() - START_TOTAL_TIME)
+        except serial.SerialException, e:
+            logging.critical(e)
+        except KeyboardInterrupt, e:
+            ser.close()
+            logging.critical(e)
+        except error_codes.TimeError:
+            logging.critical(
+                "SAMPLE_TIME and TIME_SLEEP_READ must be greater than zero. Fix in configuration file.")
+            print "Error in configuration file"
+        except error_codes.PathError:
+            logging.critical("Invalid save path. Fix in configuration file.")
+            print "Error in configuration file"
 
 
 if __name__ == '__main__':
-    START_TOTAL_TIME = time.time()
-    ser = serial.Serial()
-
-    try:
-        try:
-            SAVE_DATA, COMMANDS, TIMES = parse_config()
-            ser = auto_connect_device(COMMANDS["setup"], COMMANDS["send"])
-            READ_TIME_START = time.time()
-            OUTPUT = collect_data(
-                ser, COMMANDS["send"], TIMES["sample_time"],
-                determine_loop_count(TIMES["runtime"], TIMES["sample_time"]))
-            READ_TIME_TOTAL = time.time() - READ_TIME_START
-            write_file(OUTPUT, SAVE_DATA)
-            ser.close()
-            print "Total time sampled: %s" % str(READ_TIME_TOTAL)
-        except error_codes.ConnectError:
-            logging.critical("Unable to connect to device")
-            sys.exit()
-        except error_codes.ReturnError:
-            logging.critical("Exiting program")
-            sys.exit()
-        print "Total Time: %s" % (time.time() - START_TOTAL_TIME)
-
-    except serial.SerialException, e:
-        logging.critical(e)
-    except KeyboardInterrupt, e:
-        ser.close()
-        logging.critical(e)
-    except error_codes.TimeError:
-        logging.critical(
-            "SAMPLE_TIME and TIME_SLEEP_READ must be greater than zero. Fix in configuration file.")
-        print "Error in configuration file"
-    except error_codes.PathError:
-        logging.critical("Invalid save path. Fix in configuration file.")
-        print "Error in configuration file"
+    new_test = Test()
+    new_test.run_full()
